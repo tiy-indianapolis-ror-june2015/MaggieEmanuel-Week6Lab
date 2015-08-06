524ff883b039bede11bd6813c68f75c2d1563759
maggie-bingham <mbingham4309@gmail.com>
2015-08-06 16:32:49 -0400

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home_page#home'
+
+
+
   resources :posts
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
