13fe6ad3e36b057053d37e200f094ef1a4f19948
maggie-bingham <mbingham4309@gmail.com>
2015-08-10 14:20:50 -0400

    small change to models user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,7 @@ class User < ActiveRecord::Base
    acts_as_follower
 
 
-   def self.timeline(user)
-     following_ids = user.all_following.map(&:id)
-     all_ids = following_ids << user.id
-     Post.where(user_id: all_ids).order("created_at DESC")
-   end
+   
 
 
 
