0f1861a4a73aef20821dccf7b1afe9f434642555
maggie-bingham <mbingham4309@gmail.com>
2015-08-10 13:51:09 -0400

    modified user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -30,16 +30,12 @@ class UserController < ApplicationController
   end
 
   def follow
-    @user = User.find(:id)
+    @user = User.find(:followable_id)
       if current_user == @user
         render "You cannot follow yourself"
       else
         current_user.follow(@user)
-<<<<<<< HEAD
         render "You are now following #{@user}"
-=======
-        render "You are now following #{@user.username}"
->>>>>>> 784a090dc4c237dae351b87dba126bac54eb774d
       end
       redirect_to :back
   end
