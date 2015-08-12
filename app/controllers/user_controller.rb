445498e658d535ba2fb18c7ef7812dee7f7eeec0
maggie-bingham <mbingham4309@gmail.com>
2015-08-12 16:09:21 -0400

    modified follow methods

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 
 class UserController < ApplicationController
-  attr_accessor :email, :username, :user_bio, :user_photo
+  attr_accessor :email, :user_name, :user_bio, :user_photo
 
   def new
     @user = User.new
@@ -23,24 +23,27 @@ class UserController < ApplicationController
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
   def follow
 
     @user = User.find(params[:id])
-      if current_user == @user
-      flash[:notice] = "You are now following."
+      if current_user
+        current_user == @user
+        flash[:notice] = "You cannot follow yourself"
       else
         current_user.follow(@user)
+        flash[:notice] = "You are now following"
       end
-      redirect_to :back
+      redirect_to :posts
   end
 
   def unfollow
     @user = User.find(params[:id])
-        current_user.stop_following(@user)
-        redirect_to root_path
+    current_user.stop_following(@user)
+    flash[:notice] = "You are no longer following"
+    redirect_to root_path
   end
 
 end
